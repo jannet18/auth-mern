a350ef054e94b2dc595184573e5202e7bd145762
jannet18 <janetngei18@gmail.com>
2024-01-25 14:50:19 +0300

    header update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
           <Link to="/profile">
             {currentUser ? (
               <img
-                src={currentUser?.profilePicture || profilePicture}
+                src={currentUser?.profilePicture || current.profilePicture}
                 alt="profile"
                 className="h-7 w-7 rounded-full object-cover"
               />
